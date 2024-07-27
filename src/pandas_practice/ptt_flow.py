c82c7c4c81317d523ef7fda821cd96bc6d664089
Tammy <ttlin0602@gmail.com>
2024-07-27 15:17:19 +0800

    Update: class2-1

--- src/pandas_practice/ptt_flow.py
+++ src/pandas_practice/ptt_flow.py
@@ -11,25 +11,28 @@ COLUMNS = [
     "時間",
 ]
 
+
 # Define functions
 def get_text_file_paths() -> list[str]:
-    return glob.glob(
-        "/workspaces/data_pipeline/res_gossiping/*.txt"
-    )
+    return glob.glob("/workspaces/Datapipline/resource/res_gossiping/*.txt")
+
 
 def e_text_file(path: str) -> str:
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
 
+
 def t_text_to_df_row_list(article_string: str) -> list[str]:
     reply_info_string = article_string.split("---split---")[1]
     return reply_info_string.split("\n")[1:-1]
+
 
 def t_conbine_list_to_df(reply_info_lists: list[list]) -> pd.DataFrame:
     return pd.DataFrame(
         data=reply_info_lists,
         columns=COLUMNS,
     )
+
 
 def l_df_to_csv(df: pd.DataFrame) -> None:
     df.to_csv("ptt_2.csv", index=0)
